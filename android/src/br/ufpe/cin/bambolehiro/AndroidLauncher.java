7839faab0e0bc9a88def71d62bf8dd69cb8a4c53
igor-simoes <isol2@cin.ufpe.br>
2018-10-24 16:49:19 -0300

    libgdx initial game

---
+++ android/src/br/ufpe/cin/bambolehiro/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.ufpe.cin.bambolehiro;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.ufpe.cin.bambolehiro.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/br/ufpe/cin/bambolehiro/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.ufpe.cin.bambolehiro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.ufpe.cin.bambolehiro.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
